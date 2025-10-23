5d85da809cfbd843a7a21a54a483d241ca8ae555
li888 <alexlibaobao@gmail.com>
2025-10-23 09:29:09 -0400

    Updated themes and relevant project & links

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import AboutMe from './components/aboutme/AboutMe';
 import LandingPage from './components/landing/LandingPage';
 import { Card, Navbar, Nav } from 'react-bootstrap';
-import monkeyImage from './assets/monkey.jpg';
 
 function App() {
   return (
@@ -11,9 +10,6 @@ function App() {
       <Navbar className="flex-column custom-navbar">
         <Nav className="flex-column custom-nav">
           <Navbar.Brand href="#home" className="custom-brand">
-            <img
-              src = {monkeyImage} width = "50" height = "50">
-            </img>
           </Navbar.Brand>
           <Nav.Link href="#about" className="custom-link">About</Nav.Link>
           <Nav.Link href="#skills" className="custom-link">Skills</Nav.Link>
--- src/components/aboutme/AboutMe.jsx
+++ src/components/aboutme/AboutMe.jsx
@@ -6,15 +6,13 @@ import './AboutMe.css'; // Ensure this path is correct
 import { FaEnvelope, FaLinkedin, FaGithub, FaFileAlt } from 'react-icons/fa';
 
 
-import profileImage from '../../assets/profile.png'; // Ensure this path is correct
-import dataDashboard from '../../assets/dataDashboard.png';
-import tables4u from '../../assets/tables4u.png';
+import profileImage from '../../assets/profile.PNG'; // Ensure this path is correct (match disk casing)
+import iqp from '../../assets/iqp.png';
 import mhaccImage from '../../assets/MHACC.png';
 import mmumage from '../../assets/mmu.png';
-import flightPath from '../../assets/delta.png';
 import songProject from '../../assets/songProject.png';
 import wpi from '../../assets/wpilogo.png';
-import radicalAI from '../../assets/radicalAI.png';
+import almaAI from '../../assets/almaAI.png';
 
 // import catPlaceholder from '../../assets/funny-dancing-cats-fb3-png__700.jpg';
 
@@ -46,7 +44,7 @@ const AboutMe = () => {
               <p>
                 You can click to see my
                 <a
-                  href="https://drive.google.com/file/d/1JFIr94rAh55e3Ejy0-m2L6_DnPeRewej/view?usp=sharing"
+                  href="https://drive.google.com/file/d/1_o-CztX0B52nUlfUIA1mELn4QuwzCmAw/view?usp=sharing"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
@@ -74,16 +72,18 @@ const AboutMe = () => {
               <div className="experienceContainer" style={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap' }}>
 
                 <Card className="card-hover" style={{ width: '20rem', height: '23rem', margin: '5px', backgroundColor: 'black', border: '1px solid white' }}>
-                  <Card.Img variant="top" src="https://play-lh.googleusercontent.com/-dOP-JzLHyBgutUvCeYcXH1SAT0yLOMS0t8GS30Z42OohzhAw2Yyf9wQ2j8Aui8Cjdc" style={{ height: '100px', objectFit: 'contain', background: '#007142' }} />
+                  <Card.Img variant="top" src={almaAI} style={{ height: '100px', objectFit: 'contain', background: '#007142' }} />
                   <br />
                   <Card.Body>
                     <Card.Title style={{ color: '#D1D5DB' }}><b> Software Engineer Intern @ Fidelity Investments </b> <span style={{ color: '#9CA3AF', fontSize: '0.9em' }}></span></Card.Title>
-                    <Card.Text style={{ color: '#9CS3AF', textAlign: 'left' }}>
-                      - Streamlined app creation-to-deployment with custom NX generators and Nunjucks templates, reducing a 2-day manual process to 5 minutes; led cross-team evaluations of multiple approaches and secured stakeholder buy-in.
+                    <Card.Text style={{ color: '#9CA3AF', textAlign: 'left' }}>
+                      - Contributed to the integration and deployment of TxGemma to an in-house drug discovery application, enabling real-world therapeutic reasoning tasks across drug labeling, safety, and regulatory challenges.
                       <br /><br />
-                      - Led an effort to automate migrating end-to-end tests from Cypress to Playwright for internal applications, improving testing efficiency and reducing flakiness.
+                      - Leveraged the model’s interactive reasoning capabilities to therapeutic development pipelines, enabling predictive tasks such as adverse event detection, molecular property explanation, and regulatory risk assessment.
+
                       <br /><br />
-                      - Initiated research and developed Proofs of Concept for integrating Model Context Protocol into the internal portfolio management tool, delivering key technical documentation and analysis to inform strategic decisions.
+                     - Implemented frontend integration pipelines for TxGemma using React, TypeScript, and Flask APIs, enabling real-time visualization of model outputs, including molecular embeddings, toxicity predictions, and confidence metrics.
+
                     </Card.Text>
                     <br />
                   </Card.Body>
@@ -98,7 +98,7 @@ const AboutMe = () => {
                       - Provided support for undergraduate courses by holding weekly 3-hour office hours, assisting in lab sections, and grading
                       approximately 90 homework submissions bi-weekly.
                       <br /><br />
-                      - Ensured fair and consistent evaluation of student performance.
+                      - Delivered detailed code reviews and feedback on coding style, efficiency, and correctness.
                       <br /><br />
                       - Developed supplementary materials and tutorials to enhance student understanding of course concepts.
                     </Card.Text>
@@ -106,21 +106,7 @@ const AboutMe = () => {
                   </Card.Body>
                 </Card>
 
-                <Card className="card-hover" style={{ width: '20rem', height: '23rem', margin: '5px', backgroundColor: 'black', border: '1px solid white' }}>
-                  <Card.Img variant="top" src={radicalAI} style={{ height: '80px', objectFit: 'cover' }} />
-
-                  <Card.Body>
-                    <Card.Title style={{ color: '#D1D5DB' }}><b> Software Engineer @ Radical AI </b></Card.Title>
-                    <Card.Text style={{ color: '#9CS3AF', textAlign: 'left' }}>
-                      - Increased user engagement by 70% by leveraging React (JavaScript) and Redux to overhaul the chat discussion feature, implementing a more intuitive and responsive user interface.
-                      <br /><br />
-                      - Collaborated closely with AI and Product teams, leveraging Agile Scrum methodologies (including daily stand-ups and sprint planning) to ensure seamless integration and timely deployment of new features.
-                      <br /><br />
-                      - Demonstrated proficiency in Firebase, Vercel, React.js, Node.js, Express.js, Python, and Git for version control.
-                    </Card.Text>
-                    <br />
-                  </Card.Body>
-                </Card>
+              
               </div>
             </section>
           </div>
@@ -130,44 +116,20 @@ const AboutMe = () => {
               <h1>Projects</h1>
               <div className="projects-container" style={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap' }}>
                 <Card className="card-hover" style={{ width: '20rem', height: '35rem', margin: '5px', backgroundColor: 'black', border: '1px solid white' }}>
-                  <Card.Img variant="top" src={dataDashboard} style={{ height: '150px', objectFit: 'cover', background: '#fff' }} />
+                  <Card.Img variant="top" src={iqp} style={{ height: '150px', objectFit: 'cover', background: '#fff' }} />
                   <br />
                   <Card.Body>
-                    <Card.Title style={{ color: '#D1D5DB' }}><b> EMERGENCY DATA DASHBOARD </b> <span style={{ color: '#9CA3AF', fontSize: '0.9em' }}></span></Card.Title>
-                    <Card.Text style={{ color: '#9CS3AF', textAlign: 'left' }}>
-                      - Led a 4-person team to build a Flask–React dashboard with real-time resident data and alert capabilities, enabling Puerto Rican communities to customize, centralize, and share emergency preparedness guides and resources.
-                      <br /><br />
-                      - Built a WhatsApp broadcast system reaching 1,000+ residents to automate emergency communications and reduce coordination time, improving overall response efficiency before and after disasters.
-                      <br /><br />
-                      - Delivered the platform to community leaders, along with training and bilingual documentation, ensuring a smooth handoff and paving the way for future AI-driven enhancements.
-                    </Card.Text>
-                    <br />
-                    <Stack spacing={2} direction="row" justifyContent="center">
-                      <Button variant="contained" href="https://github.com/EricLi3/EmergencyDataDashboard">GitHub</Button>
-                      <Button variant="contained" href="https://github.com/EricLi3/automatingBroadcasts">GitHub</Button>
-                    </Stack>
-                  </Card.Body>
-                </Card>
+                    <Card.Title style={{ color: '#D1D5DB' }}><b> Digital Twin of France’s Energy Market </b> <span style={{ color: '#9CA3AF', fontSize: '0.9em' }}></span></Card.Title>
+                    <Card.Text style={{ color: '#9CA3AF', textAlign: 'left' }}>
+                      - Led a 5-person team in developing a digital twin to simulate residential energy consumer behavior in France, using real-world survey and market data from EDF and CRE. Predicted consumer switching behavior and policy impacts, achieving high model fidelity (average loss &lt;0.02%). 
 
-                <Card className="card-hover" style={{ width: '20rem', height: '35rem', margin: '5px', backgroundColor: 'black', border: '1px solid white' }}>
-                  <Card.Img variant="top" src={tables4u} style={{ height: '150px', objectFit: 'cover' }} />
-                  <br />
-                  <Card.Body>
-                    <Card.Title style={{ color: '#D1D5DB' }}><b> TABLES4U </b></Card.Title>
-                    <Card.Text style={{ color: '#9CS3AF', textAlign: 'left' }}>
-                      - Collaborated in a team of 4 to create a full-stack restaurant management system, including use case definitions, UML diagrams, and a MySQL database through database mapping, ensuring data integrity and efficient queries.
                       <br /><br />
-                      - Modeled API architecture with YAML, developed and tested a RESTful API using AWS Lambda and API
-                      Gateway, validated JSON responses with Postman, and applied best practices for error handling and validation.
-                      <br /><br />
-                      - Built a scalable Next.js frontend using Typescript with JWT-based authentication, page-based routing, and
-                      deployed the application via AWS S3, collaborating on UI design using Figma.
+                      - Built a frontend dashboard using Next.js, TypeScript, D3.js, and Tailwind CSS, integrating it with a Julia backend via a custom HTTP server to enable real-time scenario execution and interactive policy impact visualization.
+
+
                     </Card.Text>
                     <br />
-                    <Stack spacing={2} direction="row" justifyContent="center">
-                      <Button variant="contained" href="https://github.com/EricLi3/tables4u">GitHub</Button>
-                      <Button variant="contained" href="https://tables4u.vercel.app/">Website</Button>
-                    </Stack>
+
                   </Card.Body>
                 </Card>
 
@@ -176,7 +138,7 @@ const AboutMe = () => {
                   <br />
                   <Card.Body>
                     <Card.Title style={{ color: '#D1D5DB' }}><b> MUSIC DISCOVERY APP </b></Card.Title>
-                    <Card.Text style={{ color: '#9CS3AF', textAlign: 'left' }}>
+                    <Card.Text style={{ color: '#9CA3AF', textAlign: 'left' }}>
                       - Developed a <b>REST API</b> using Spring Boot and MongoDB, including 20+ endpoints, and tested it with Postman.
                       <br /><br />
                       - Built a responsive user interface using <b>React/MUI, Bootstrap, and CSS </b>for exploring, rating, and commenting on 200+ songs. Integrating the <b>Last.FM API</b> to fetch song information
@@ -185,8 +147,8 @@ const AboutMe = () => {
                     </Card.Text>
                     <br />
                     <Stack spacing={2} direction="row" justifyContent="center">
-                      <Button variant="contained" href="https://github.com/EricLi3/Music-Discovery-API">GitHub</Button>
-                      <Button variant="contained" href="http://songchatroom-react-app.s3-website.us-east-2.amazonaws.com/">Website</Button>
+                      {/* <Button variant="contained" href="https://github.com/EricLi3/Music-Discovery-API">GitHub</Button>
+                      <Button variant="contained" href="http://songchatroom-react-app.s3-website.us-east-2.amazonaws.com/">Website</Button> */}
                     </Stack>
                   </Card.Body>
                 </Card>
@@ -196,8 +158,8 @@ const AboutMe = () => {
                   <br />
                   <Card.Body>
                     <Card.Title style={{ color: '#D1D5DB' }}><b> ASIAN MENTAL HEALTH PLATFORM </b> <br /><i>In association with the Mental Health Association for Chinese Communities</i></Card.Title>
-                    <Card.Text style={{ color: '#9CS3AF', textAlign: 'left' }}>
-                      - Led a <b>3-person team</b> to design and implement a platform in <b>7 weeks</b>. Implemented a user-friendly experience using <b>HTML, CSS, and JS.</b> Deployed the site on <b>Firebase</b> and implemented <b>CI/CD using GitHub Actions.</b>
+                    <Card.Text style={{ color: '#9CA3AF', textAlign: 'left' }}>
+                      - Worked on a <b>3-person team</b> to design and implement a platform in <b>7 weeks</b>. Implemented a user-friendly experience using <b>HTML, CSS, and JS.</b> Deployed the site on <b>Firebase</b> and implemented <b>CI/CD using GitHub Actions.</b>
                       <br /><br />
                       - Developed a chatbot with <b>IBM WatsonX</b> incorporating <b>10 intents and 5 entities</b> to connect users to mental health professionals and resources. Also provided <b>multilingual guides</b> on mental health stigma using the <b>Google Translate API.</b>
                       <br /><br />
@@ -205,31 +167,11 @@ const AboutMe = () => {
 
                     </Card.Text>
                     <br />
-
+{/* 
                     <Stack spacing={2} direction="row" justifyContent="center">
                       <Button variant="contained" href="https://github.com/EricLi3/Contemporary-Chinese-Final-Project">GitHub</Button>
                       <Button variant="contained" href="https://contemporary-chinese-final-project.vercel.app/">Website</Button>
-                    </Stack>
-                  </Card.Body>
-                </Card>
-
-                <Card className="card-hover" style={{ width: '20rem', height: '30rem', margin: '5px', backgroundColor: 'black', border: '1px solid white' }}>
-                  <Card.Img variant="top" src={flightPath} style={{ height: '150px', objectFit: 'cover' }} />
-                  <br />
-                  <Card.Body>
-                    <Card.Title style={{ color: '#D1D5DB' }}><b> TRANSPORTATION NETWORK ANALYSIS </b></Card.Title>
-                    <Card.Text style={{ color: '#9CS3AF', textAlign: 'left' }}>
-                      - Analyzed Delta and Southwest flight networks, processing <b>10,000+ routes</b> with <b>BFS</b> and <b>Floyd-Warshall</b>. Created a tool
-                      for <b>shortest, longest, and average flight distances</b>, and determined the <b>longest and shortest path</b> between airports.
-                      <br /><br />
-
-                      - Developed a <b>pathfinding system</b> on a <b>10,796-vertex, 12,986-edge U.S. highway network</b>, comparing the <b>effectiveness</b> of <b>BFS, DFS, and Dijkstra’s</b> algorithms for pathfinding
-                    </Card.Text>
-                    <br />
-
-                    <Stack spacing={2} direction="row" justifyContent="center">
-                      <Button variant="contained" href="https://github.com/EricLi3/CS2223-HW-assignments/blob/main/ekli-HW4.zip">GitHub</Button>
-                    </Stack>
+                    </Stack> */}
                   </Card.Body>
                 </Card>
 
@@ -238,14 +180,14 @@ const AboutMe = () => {
                   <br />
                   <Card.Body>
                     <Card.Title style={{ color: '#D1D5DB' }}><b> VIRTUAL MEMORY MANAGER SIMULATION </b></Card.Title>
-                    <Card.Text style={{ color: '#9CS3AF', textAlign: 'left' }}>
+                    <Card.Text style={{ color: '#9CA3AF', textAlign: 'left' }}>
                       - Implemented a <b>memory manager in C</b>, simulating virtual memory with <b>paging and swapping</b>, including <b>address translation</b>, per-process <b>page tables</b>, and efficient memory operations within a <b>64-byte simulated</b> physical memory
                       <br /><br />
                       - Enhanced system capabilities by supporting <b>disk swapping</b> with a robust <b>round-robin eviction</b> strategy, ensuring process isolation and resource efficiency
 
                     </Card.Text>
                     <Stack spacing={2} direction="row" justifyContent="center">
-                      <Button variant="contained" href="https://github.com/EricLi3/Operating-Systems/tree/main/Projects/Project%203">GitHub</Button>
+                      {/* <Button variant="contained" href="https://github.com/EricLi3/Operating-Systems/tree/main/Projects/Project%203">GitHub</Button> */}
                       {/* <Button variant="contained" href="https://contemporary-chinese-final-project.vercel.app/">Website</Button> */}
                     </Stack>
                   </Card.Body>
@@ -259,19 +201,19 @@ const AboutMe = () => {
 
           <div className="footer">
             <section id="contact">
-              <h2 class="title"> Contact Me </h2>
+                <h2 className="title"> Contact Me </h2>
 
               <div className="contacts">
-                <a href="mailto:alexli@wpi.edu" target="_blank" rel="noopener noreferrer" className="icon-container email">
+                <a href="mailto:ajli@wpi.edu" target="_blank" rel="noopener noreferrer" className="icon-container email">
                   <FaEnvelope size={40} />
                 </a>
-                <a href="https://www.linkedin.com/in/alex-li" target="_blank" rel="noopener noreferrer" className="icon-container linkedin">
+                <a href="https://www.linkedin.com/in/alex-jianqiao-li" target="_blank" rel="noopener noreferrer" className="icon-container linkedin">
                   <FaLinkedin size={40} />
                 </a>
-                <a href="https://github.com/alexli" target="_blank" rel="noopener noreferrer" className="icon-container github">
+                <a href="https://github.com/alexli888" target="_blank" rel="noopener noreferrer" className="icon-container github">
                   <FaGithub size={40} />
                 </a>
-                <a href="https://drive.google.com/file/d/1JFIr94rAh55e3Ejy0-m2L6_DnPeRewej/view?usp=sharing" target="_blank" rel="noopener noreferrer" className="icon-container resume">
+                <a href="https://drive.google.com/file/d/1_o-CztX0B52nUlfUIA1mELn4QuwzCmAw/view?usp=sharing" target="_blank" rel="noopener noreferrer" className="icon-container resume">
                   <FaFileAlt size={40} />
                 </a>
               </div>
